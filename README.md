79989cc61ba816308d82579d1aaab56fb7587212
Jordi Beltran <jordi.beltran-querol.external@capgemini.com>
2025-05-05 12:37:31 +0200

    Pruebas en ProductTitle

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Este es un paquete de pruebas de despliegue en NPM
 
 ## Ejemplo
 ```
-import {} from 'jb-product-card'
+import {ProductCard, ProductImage, ProductTitle, ProductButtons} from 'jb-product-card'
 ```
 
 ```
--- example/index.tsx
+++ example/index.tsx
@@ -1,40 +1,33 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {ProductCard, ProductImage, ProductTitle, ProductButtons} from '../.'
-import { products } from '../src/data/products';
+import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../.';
 
-const product = products[0];
+const product = {
+  id: '1',
+  title: 'Coffee Mug - Card!',
+  // img: './coffee-mug.png'
+}
 
 const App = () => {
   return (
     <>
       <ProductCard 
-        product={product}
-
-        // Patrón StateInitializer
-        initialValues={{
-            count: 4,
-            maxCount: 10
-        }}
+          product={ product }
+          initialValues={{
+              count: 6,
+              // maxCount: 10,
+          }}
       >
-        {
-            ({reset, count, increaseBy, isMaxCountReached, maxCount}) => (
-              <>
-                <ProductCard.Image/>
-                <ProductCard.Title/>
-                <ProductCard.Buttons/>
-
-                {/* <button onClick={reset}>Reset</button> */}
-                {/* <button onClick={()=>increaseBy(-2)}>-2</button> */}
-                {/* Si no se llega al isMaxCount, ocultar */}
-                {/* {!isMaxCountReached && <button onClick={()=>increaseBy(+2)} >+2</button>} */}
-                
-                
-                {/* <span>{count} - {maxCount}</span> */}
-              </>
-            )
-        }
+          {
+              ({ reset, count, isMaxCountReached, maxCount, increaseBy  }) => (
+                  <>
+                      <ProductImage />
+                      <ProductTitle />
+                      <ProductButtons />
+                  </>
+              )
+          }
       </ProductCard>
     </>
   );
